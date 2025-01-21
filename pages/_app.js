36b76aad18528e298353ec5ad7946db0f7d60cb4
srshukla2001 <129613229+srshukla2001@users.noreply.github.com>
2025-01-21 08:52:28 +0530

    favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-} 
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <title>Best AI-Powered Youtube Seo | HighPre.com</title> {/* Add your global title here */}
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
